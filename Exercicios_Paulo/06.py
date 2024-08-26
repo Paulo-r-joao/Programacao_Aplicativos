3a4c462421f27442c744672d6a0c0758bcbf3b88
Paulo-r-joao <paulo_r_joao@estudante.sesisenai.org.br>
2024-08-26 16:26:20 -0300

    commit

--- Exercicios_Paulo/06.py
+++ Exercicios_Paulo/06.py
@@ -2,8 +2,8 @@ def converter_quilometros_para_metros(quilometros):
     metros = quilometros * 1000
     return metros
 try:
-quilometros = float(input(“Digite a distância em quilômetros: “))
-metros = converter_quilometros_para_metros(quilometros)
-print(“metros:”, metros)
+    quilometros = float(input("Digite a distância em quilômetros: "))
+    metros = converter_quilometros_para_metros(quilometros)
+    print("metros:", metros)
 except ValueError:
-print(“Entrada inválida!”)
+    print("Entrada inválida!");
