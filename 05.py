f769978818cfc0351347594f9ca2991ac2497265
Paulo-r-joao <paulo_r_joao@estudante.sesisenai.org.br>
2024-11-01 15:53:48 -0300

    Delete 05.py

--- 05.py
+++
@@ -1,8 +0,0 @@
-from tkinter.filedialog import askopenfilename
-import pandas as pd
-
-caminho_arquivo = askopenfilename(title='Selecione um arquivo em exel para abrir')
-
-df = pd.read_excel(caminho_arquivo)
-
-print(df)
